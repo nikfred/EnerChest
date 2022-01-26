15dc77e73416d204f8ddbe914560ef1b3b43a929
nikfred <priymak678@gmail.com>
2022-01-26 17:40:16 +0200

    Fix get user data

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = observer(() => {
     useEffect(() => {
         check().then(data => {
             console.log(data)
-            user.setUser(true)
+            user.setUser(data)
             user.setIsAuth(true)
             user.setIsAdmin(data.role === 'ADMIN')
         }).finally(() => setLoading(false))
