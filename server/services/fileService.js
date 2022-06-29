9f6159c4f1096d4f02d776fdb28dfc34522e6573
nikfred <priymak678@gmail.com>
2022-06-29 22:25:42 +0300

    fix fileService

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -1,8 +1,6 @@
 const uuid = require('uuid')
 const path = require('path')
 const fs = require('fs')
-const Product = require('../models/product')
-const User = require('../models/user')
 
 class FileService {
     async save(file, type) {
@@ -20,46 +18,11 @@ class FileService {
     async remove(fileUrl) {
         const [type, url] = fileUrl.split('/')
         const filePath = path.resolve(__dirname, '../static', type)
-        return fs.unlinkSync(path.resolve(filePath, url))
-    }
-
-    async disciplineImage(type) {
-        let images
-        switch (type) {
-            case 'product':
-                images = await Product.find({},'imageUrl')
-                break
-            case 'user':
-                images = await User.find({},'imageUrl')
-                break
-        }
-        images = images
-            .filter(i => i.imageUrl.indexOf(type))
-            .map(i => i.imageUrl)
-
-        const newFilePath = (path.resolve(__dirname, '../static', type)) + path.sep
-        if (!fs.existsSync(newFilePath)) {
-            fs.mkdirSync(newFilePath, {recursive: true})
-        }
-        const oldFilePath = path.resolve(__dirname, '../static') + path.sep
-        console.log(images)
-        for (const image of images) {
-            console.log(oldFilePath + image)
-            if (fs.existsSync(oldFilePath + image)) {
-                fs.renameSync(oldFilePath + image, newFilePath + image, (err =>
-                        console.log(err)
-                ))
-                switch (type) {
-                    case 'product':
-                        await Product.updateOne({imageUrl: image},{imageUrl: type + '/' + image})
-                        break
-                    case 'user':
-                        await User.updateOne({imageUrl: image},{imageUrl: type + '/' + image})
-                        break
-                }
-            }
-        }
-        return true
+        let isExist
+        console.log(path.resolve(filePath, url))
+        isExist = fs.existsSync(path.resolve(filePath, url))
+        console.log(`isExist = ${isExist}`)
+        return isExist ? fs.unlinkSync(path.resolve(filePath, url)) : 0
     }
 }
 
